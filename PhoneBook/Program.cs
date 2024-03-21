b63b166ad5b475fcdd970d41d3350fee12e9109a
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2024-03-21 16:47:56 +0500

    INFRA: Initialize Project

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using PhoneBook.Models.Contacts;
+using PhoneBook.Services;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
